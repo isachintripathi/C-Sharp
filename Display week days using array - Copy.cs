7aabcf96a6d2130ee099ae2a6417971da63e4b8e
Sachin Tripathi <65774461+itripathisachin@users.noreply.github.com>
2021-01-27 02:57:44 +0530

    Fundamentals of C#

---
+++ Display week days using array - Copy.cs
@@ -0,0 +1,28 @@
+﻿// // C# program to creating an array 
+// // of the string as week days, store  
+// // day values in the weekdays, 
+// // and prints each value. 
+// using System;
+// namespace Arrays
+// {
+
+    // class ArraysOfString
+    // {
+
+        // // Main Method 
+        // public static void Main()
+        // {
+
+            // // declares a 1D Array of string. 
+            // string[] weekDays;
+
+            // // allocating memory for days. 
+            // weekDays = new string[] {"Sun", "Mon", "Tue", "Wed",
+                                       // "Thu", "Fri", "Sat"};
+
+            // // Displaying Elements of array 
+            // foreach (string day in weekDays)
+                // Console.WriteLine(day + " ");
+        // }
+    // }
+// }
---
+++ Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+1. Dictionary Consists of Key value Pair. key > datatype , Value > object 
+2. We can get, add, count, remove & clear records which are in Dictionary
+3. We can convert any array or List into Dictionary
+4. In order to loop Dictionary data, we have to select KeyValuePair as Type
+5. We can use Linq extension to filter the records.
+6. Key in Dictionary must be unique.
+7. It provide fast lookups for values using keys.
+8. its class is present in System.Collections.Generic namespace.
+*/
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee emp1 = new Employee()
+            {
+                ID = 1,
+                Name = "Sachin",
+                Salary = 10000,
+            };
+
+            Employee emp2 = new Employee()
+            {
+                ID = 2,
+                Name = "Saurabh",
+                Salary = 15000,
+            };
+
+            Employee emp3 = new Employee()
+            {
+                ID = 3,
+                Name = "Rohit",
+                Salary = 25000,
+            };
+
+            // create a Dictionary
+            Dictionary<int, Employee> dict = new Dictionary<int, Employee>();
+
+            // add data to dictionary
+            dict.Add(emp1.ID, emp1);
+            dict.Add(emp2.ID, emp2);
+            dict.Add(emp3.ID, emp3);
+
+            // access data from Dictionary
+            Employee emp0 = dict[3];
+
+            //print dictionary data
+            Console.WriteLine("ID = {0}, Name = {1}, Salary = {2}", emp0.ID, emp0.Name, emp0.Salary);
+            Console.WriteLine("------------------------------------------------------------------");
+
+            //in case of multiple records we use foreach using keyValue Pair
+            foreach (KeyValuePair<int, Employee> emp in dict)
+            {
+                Console.WriteLine("ID = {0}", emp.Key);
+                Console.WriteLine("ID = {0}, Name = {1}, Salary = {2}", emp.Value.ID, emp.Value.Name, emp.Value.Salary);
+            }
+
+            Console.WriteLine("--------------------------Print Key Value both With Key Value Pair-----------------------------------------");
+
+            foreach (KeyValuePair<int, Employee> empp in dict)
+            {
+                int ee = empp.Key;
+                Employee e = empp.Value;
+                Console.WriteLine("ID = {0}", ee);
+                Console.WriteLine("ID = {0}, Name = {1}, Salary = {2}", e.ID, e.Name, e.Salary);
+            }
+
+            Console.WriteLine("--------------------------Print Value Without Key Value Pair-----------------------------------------");
+
+            foreach (var emp in dict.Values)
+            {
+                Console.WriteLine("ID = {0}, Name = {1}, Salary = {2}", emp.ID, emp.Name, emp.Salary);
+            }
+
+            Console.WriteLine("---------------------Print Key Without Key Value Pair---------------------------------------------");
+
+            foreach (int emp in dict.Keys)
+            {
+                Console.WriteLine("ID = {0}", emp);
+            }
+
+            //TryGetValue
+            Console.WriteLine("---------------------TryGetValue---------------------------------------------");
+
+            Employee employee;
+            if (dict.TryGetValue(1, out employee))
+            {
+                Console.WriteLine("ID = {0}, Name = {1}, Salary = {2}", employee.ID, employee.Name, employee.Salary);
+            }
+            else
+            {
+                Console.WriteLine("Key not found");
+            }
+
+            //count
+            Console.WriteLine("---------------------count---------------------------------------------");
+
+            Console.WriteLine(" total count is : {0} ", dict.Count(kvp => kvp.Value.Salary > 10000));
+
+            //remove
+            Console.WriteLine("---------------------remove---------------------------------------------");
+
+            dict.Remove(3);
+
+            foreach (KeyValuePair<int, Employee> emp in dict)
+            {
+                Console.WriteLine("ID = {0}", emp.Key);
+                Console.WriteLine("ID = {0}, Name = {1}, Salary = {2}", emp.Value.ID, emp.Value.Name, emp.Value.Salary);
+            }
+
+            //clear
+            Console.WriteLine("---------------------clear---------------------------------------------");
+            dict.Clear();
+
+            Console.WriteLine("---------------------Array To Dictionary---------------------------------------------");
+
+            Employee[] employeeArray = new Employee[3];
+            employeeArray[0] = emp1;
+            employeeArray[1] = emp2;
+            employeeArray[2] = emp3;
+
+            Dictionary<int, Employee> dictArray = employeeArray.ToDictionary(obj => obj.ID, obj => obj);
+
+            foreach (KeyValuePair<int, Employee> array in dictArray)
+            {
+                Console.WriteLine("ID = {0}", array.Key);
+                Console.WriteLine("ID = {0}, Name = {1}, Salary = {2}", array.Value.ID, array.Value.Name, array.Value.Salary);
+            }
+
+            Console.WriteLine("---------------------List To Dictionary---------------------------------------------");
+
+            List<Employee> employeeList = new List<Employee>();
+            employeeList.Add(emp1);
+            employeeList.Add(emp2);
+            employeeList.Add(emp3);
+
+            Dictionary<int, Employee> dictList = employeeList.ToDictionary(obj => obj.ID, obj => obj);
+
+            foreach (KeyValuePair<int, Employee> list in dictArray)
+            {
+                Console.WriteLine("ID = {0}", list.Key);
+                Console.WriteLine("ID = {0}, Name = {1}, Salary = {2}", list.Value.ID, list.Value.Name, list.Value.Salary);
+            }
+        }
+
+    }
+
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+}
